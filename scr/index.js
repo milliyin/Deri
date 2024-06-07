185b2b94ff95b577f1f76d29253fa3e356c1b9ba
milliyin <webdowngaming@gmail.com>
2024-06-07 18:39:42 +0500

    Basic of the bots

---
+++ scr/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Client, IntentsBitField} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+});
+
+client.on('ready', (c) => {
+    console.log(`Logged in as ${c.user.tag}!`);
+})
+
+client.login(process.env.TOKEN);
