8d1e2130a75c0132dce7837d2a3690f9c7131b27
milliyin <webdowngaming@gmail.com>
2024-06-08 19:05:45 +0500

    There was an error registering command is the ban.js Fixes #4

--- scr/commands/moderation/ban.js
+++ scr/commands/moderation/ban.js
@@ -2,26 +2,26 @@
 
 module.exports = {
     deleted: true,
-    name: 'Ban',
+    name: 'ban',
     description: 'Ban a memeber from the server!',
     // devOnly: Boolean,
-    // testOnly: Boolean,
+    testOnly: true,
     
-    //  Options: [
-    //     {
-    //         name : 'target-user',
-    //         description: 'the user to ban.',
-    //         required: true,
-    //         type: ApplicationCommandOptionType.Mentionable,
-    //     },
-    //     {
-    //         name : 'reason',
-    //         description: 'the reason for banning.',
-    //         type: ApplicationCommandOptionType.String,
-    //     }  
-    // ],
-    // PermissionsRequired: [PermissionFlagsBits.Administrator],
-    // botPermissions: [PermissionFlagsBits.Administrator],
+     Options: [
+        {
+            name : 'target-user',
+            description: 'the user to ban.',
+            required: true,
+            type: ApplicationCommandOptionType.Mentionable,
+        },
+        {
+            name : 'reason',
+            description: 'the reason for banning.',
+            type: ApplicationCommandOptionType.String,
+        }  
+    ],
+    PermissionsRequired: [PermissionFlagsBits.Administrator],
+    botPermissions: [PermissionFlagsBits.Administrator],
 
     callback:( client, interaction ) => {
         interaction.reply(`ban..`);
