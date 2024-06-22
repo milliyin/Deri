32f660b4a1da3e341ecdd78b81dc8d28335527f3
milliyin <webdowngaming@gmail.com>
2024-06-22 13:43:19 +0500

    Ban command working!

--- scr/commands/moderation/ban.js
+++ scr/commands/moderation/ban.js
@@ -1,4 +1,4 @@
-const { ApplicationCommandOptionType, PermissionFlagsBits} = require('discord.js');
+const { Client, Interaction, ApplicationCommandOptionType, PermissionFlagsBits} = require('discord.js');
 
 module.exports = {
     deleted: true,
@@ -20,10 +20,51 @@ module.exports = {
             type: ApplicationCommandOptionType.String,
         }  
     ],
-    PermissionsRequired: [PermissionFlagsBits.Administrator],
-    botPermissions: [PermissionFlagsBits.Administrator],
+    PermissionsRequired: [PermissionFlagsBits.BanMembers],
+    botPermissions: [PermissionFlagsBits.BanMembers],
 
-    callback:( client, interaction ) => {
-        interaction.reply(`ban..`);
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Interaction} interaction 
+     */
+
+    callback: async(client, interaction ) => {
+        const targetUserId = interaction.options.get('target-user').value;
+        const reason = interaction.options.get('reason')?.value || 'No reason provided';
+        await interaction.deferReply();
+        const targetUser = await interaction.guild.members.fetch(targetUserId);
+        if(!(interaction.member.id === interaction.guild.ownerId)){
+            
+        if(!targetUser){
+            await interaction.editReply('User not found');
+            return;
+        }
+        if( targetUser === interaction.guild.ownerId){
+            await interaction.editReply('Cant ban the owner');
+            return;
+        }
+
+        const targetUserRolePostion = targetUser.roles.highest.position;
+        const requestUserRolePostion = interaction.member.roles.highest.position;
+        const botRolePosition = interaction.guild.members.me.roles.highest.position;
+
+        if(targetUserRolePostion >= requestUserRolePostion){
+            await interaction.editReply('You cant ban this user');
+            return;
+        }
+        if(targetUserRolePostion >= botRolePosition){
+            await interaction.editReply('I cant ban this user');
+            return;
+        }
+        //baning the user;
+    }
+    try{
+        await targetUser.ban({reason});
+        await interaction.editReply(`Banned ${targetUser.user.tag} for ${reason}`)
+    }catch(error){
+        console.log(`Errror was there while banning: ${error}`);
+    }
+        
     },
 };
